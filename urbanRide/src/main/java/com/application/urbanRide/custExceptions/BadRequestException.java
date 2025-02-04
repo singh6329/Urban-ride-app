ca04ffc8faa9d188ade389e303ad41289c747e48
Jagjeet Singh <thethijagjit2000@gmail.com>
2025-02-05 02:05:38 +0530

    Added some custom exceptions

--- urbanRide/src/main/java/com/application/urbanRide/custExceptions/BadRequestException.java
+++ urbanRide/src/main/java/com/application/urbanRide/custExceptions/BadRequestException.java
@@ -3,6 +3,7 @@ package com.application.urbanRide.custExceptions;
 public class BadRequestException extends RuntimeException{
 
     public BadRequestException(String message) {
+
         super(message);
     }
 }
