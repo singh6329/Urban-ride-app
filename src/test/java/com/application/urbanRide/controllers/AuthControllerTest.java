993b26bebc2a050177e6f0725833ecd87578d927
Jagjeet Singh <thethijagjit2000@gmail.com>
2025-02-05 16:12:20 +0530

    Added some more configurations

---
+++ src/test/java/com/application/urbanRide/controllers/AuthControllerTest.java
@@ -0,0 +1,82 @@
+package com.application.urbanRide.controllers;
+
+import com.application.urbanRide.TestContainerConfiguration;
+import com.application.urbanRide.dtos.OnboardDriverDto;
+import com.application.urbanRide.dtos.SignupDto;
+import com.application.urbanRide.entities.User;
+import com.application.urbanRide.entities.enums.Roles;
+import com.application.urbanRide.repositories.RiderRepository;
+import com.application.urbanRide.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@AutoConfigureWebTestClient(timeout = "100000")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestContainerConfiguration.class)
+class AuthControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RiderRepository riderRepository;
+
+    private User user;
+
+    @BeforeEach
+    void setUpEach() {
+        user = new User();
+        user.setId(1L);
+        user.setEmail("test@example.com");
+        user.setPassword("password");
+        user.setRoles(Set.of(Roles.RIDER));
+    }
+
+    @Test
+    void testSignUp_success() {
+        SignupDto signupDto = new SignupDto();
+        signupDto.setEmail("test@example.com");
+        signupDto.setName("Test name");
+        signupDto.setPassword("password");
+
+        webTestClient.post()
+                .uri("/auth/signup")
+                .bodyValue(signupDto)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody()
+                .jsonPath("$.email").isEqualTo(signupDto.getEmail())
+                .jsonPath("$.name").isEqualTo(signupDto.getName());
+    }
+
+    //    @Test
+//    @WithUserDetails("admin@gmail.com")
+    void testOnboardDriver_success() {
+
+        if (!userRepository.existsById(1L)) {
+            userRepository.save(user);
+        }
+
+        OnboardDriverDto onboardDriverDto = new OnboardDriverDto();
+        onboardDriverDto.setVehicleId("ABC123");
+
+        webTestClient
+                .post()
+                .uri("/auth/onBoardNewDriver/1")
+                .bodyValue(onboardDriverDto)
+                .exchange()
+                .expectStatus().isCreated();
+    }
+}
